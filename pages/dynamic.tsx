bc2684c13d82414ed6c63c7f9a7ee6cd3f6d5e9b
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2023-07-03 22:45:41 -0300

    corri, bug

--- pages/dynamic.tsx
+++ pages/dynamic.tsx
@@ -1,68 +1,68 @@
 import { GetServerSideProps, NextPage } from "next";
-import { ReactNode, useEffect, useState } from "react";
-import { Col, Container, Row } from "reactstrap";
+// import { ReactNode, useEffect, useState } from "react";
+// import { Col, Container, Row } from "reactstrap";
 
-interface ApiResponse {
-    name: string
-    timestamp: Date
-}
+// interface ApiResponse {
+//     name: string
+//     timestamp: Date
+// }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-	const serverSideData: ApiResponse = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
+// export const getServerSideProps: GetServerSideProps = async () => {
+// 	const serverSideData: ApiResponse = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
 
-    return {
-        props: {
-        serverSideData
-        }
-    }
-}
+//     return {
+//         props: {
+//         serverSideData
+//         }
+//     }
+// }
 
 
-const Dynamic: NextPage = (props: {
-    children?: ReactNode
-    serverSideData?: ApiResponse
-}) => {
-    const [clientSideData, setClientSideData] = useState<ApiResponse>()
+// const Dynamic: NextPage = (props: {
+//     children?: ReactNode
+//     serverSideData?: ApiResponse
+// }) => {
+//     const [clientSideData, setClientSideData] = useState<ApiResponse>()
 
-    useEffect(() => {
-        fetchData()
-    }, [])
+//     useEffect(() => {
+//         fetchData()
+//     }, [])
 
-    const fetchData = async () => {
-        const data = await fetch("/api/hello").then(res => res.json())
-        setClientSideData(data)
-    }
+//     const fetchData = async () => {
+//         const data = await fetch("/api/hello").then(res => res.json())
+//         setClientSideData(data)
+//     }
 
-    return (
-        <Container tag="main">
-            <h1 className="my-5">
-                Como funcionam as renderizações do Next.js
-            </h1>
+//     return (
+//         <Container tag="main">
+//             <h1 className="my-5">
+//                 Como funcionam as renderizações do Next.js
+//             </h1>
 
-            <Row>
-                <Col>
-                    <h3>
-                        Gerado no servidor:
-                    </h3>
-                    <h2>
-                        {props.serverSideData?.timestamp.toString()}
-                    </h2>
-                    <h2>
-                        {props.serverSideData?.name.toString()}
-                    </h2>
-                </Col>
+//             <Row>
+//                 <Col>
+//                     <h3>
+//                         Gerado no servidor:
+//                     </h3>
+//                     <h2>
+//                         {props.serverSideData?.timestamp.toString()}
+//                     </h2>
+//                     <h2>
+//                         {props.serverSideData?.name.toString()}
+//                     </h2>
+//                 </Col>
 
-                <Col>
-                    <h3>
-                        Gerado no cliente:
-                    </h3>
-                    <h2>
-                        {clientSideData?.timestamp.toString()}
-                    </h2>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+//                 <Col>
+//                     <h3>
+//                         Gerado no cliente:
+//                     </h3>
+//                     <h2>
+//                         {clientSideData?.timestamp.toString()}
+//                     </h2>
+//                 </Col>
+//             </Row>
+//         </Container>
+//     )
+// }
 
-export default Dynamic
+// export default Dynamic
