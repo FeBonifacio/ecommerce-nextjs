90eff4f7ba16d09d9aa4298262a9040b6f550c90
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2023-07-03 22:30:34 -0300

    Subindo tudo já feito.

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Create </title>
+                <meta name="description" content="Gnerated by ..."/>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>Welcome to <a href="https://nextjs.org">Next.js</a></h1>
+
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, iure similique. Deserunt, consequuntur fuga molestiae facere nihil doloremque ducimus eaque omnis? Doloremque 
+                    quas illum tempore repellat obcaecati distinctio eos repudiandae?
+                </p>
+            </main>
+        </>
+    )
+}
+
+export default Home;
